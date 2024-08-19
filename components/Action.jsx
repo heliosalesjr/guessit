14905ab38b8df1118e9559b17aabe6ab5c0d1331
heliosalesjr <65938536+heliosalesjr@users.noreply.github.com>
2024-08-19 12:19:21 -0300

    navbar & main content updated

--- components/Action.jsx
+++ components/Action.jsx
@@ -6,10 +6,10 @@ export default function Action() {
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gradient-to-r from-purple-600 to-pink-600 text-center p-8">
       <h1 className="text-5xl font-extrabold text-white mb-4 tracking-wide">
-        Este é o jogo! 
+      Descubra o Número Secreto!
       </h1>
       <p className="text-lg text-white max-w-lg mb-6">
-        Entre em um jogo, crie um jogo, desafie seus limites!
+      Reúna seus amigos, crie um jogo e veja quem consegue desvendar o mistério primeiro. Adivinhe o número certo com menos tentativas e alcance o topo do placar. Você está preparado para o desafio?
       </p>
       <div className="flex gap-4">
         <button className="px-6 py-3 bg-purple-400 text-black font-bold rounded-lg hover:bg-pink-300 transition-colors">
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -7,24 +7,29 @@ export default function MyNav() {
   return (
     <Navbar className="flex py-4 px-4">
       <NavbarContent className="flex justify-between w-full">
-        <NavbarBrand>
-          <p className="font-bold text-inherit">GuessIt</p>
-        </NavbarBrand>
+      <NavbarBrand className="flex items-center">
+  <img
+    src="/logo2.jpeg"
+    alt="Logo"
+    className="w-[60px] h-[60px] rounded-full mr-3" 
+  />
+  <p className="font-bold text-inherit">GuessIt</p>
+</NavbarBrand>
 
         <NavbarContent className="flex justify-center gap-4">
           <NavbarItem>
-            <Link color="foreground" href="#">
-              Features
+            <Link color="foreground" href="#" className="p-4 rounded-xl hover:bg-violet-200">
+              Como Funciona
             </Link>
           </NavbarItem>
           <NavbarItem isActive>
-            <Link href="#" aria-current="page">
-              Customers
+            <Link href="#" aria-current="page" className="p-4 rounded-xl hover:bg-violet-200">
+              Quem somos
             </Link>
           </NavbarItem>
           <NavbarItem>
-            <Link color="foreground" href="#">
-              Integrations
+            <Link color="foreground" href="#" className="p-4 rounded-xl hover:bg-violet-200">
+              Integrações
             </Link>
           </NavbarItem>
         </NavbarContent>
@@ -35,7 +40,7 @@ export default function MyNav() {
           </NavbarItem>
           <NavbarItem>
             <Button as={Link} color="primary" href="#" variant="flat">
-              Sign Up
+              Cadastro
             </Button>
           </NavbarItem>
         </NavbarContent>
